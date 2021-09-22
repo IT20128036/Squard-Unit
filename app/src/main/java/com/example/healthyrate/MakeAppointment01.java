2f966018721567159e001d85a186516be65f3185
IT20127046 <it20127046@my.sliit.lk>
2021-09-22 13:15:03 +0530

    I add Make Appointment Activity

---
+++ app/src/main/java/com/example/healthyrate/MakeAppointment01.java
@@ -0,0 +1,14 @@
+package com.example.healthyrate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeAppointment01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_appointment01);
+    }
+}
